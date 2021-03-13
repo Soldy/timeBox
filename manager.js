4f414faacf9fec17f6bf5698eefacd044dac2842
Soldy <4786022+Soldy@users.noreply.github.com>
2021-03-13 09:29:10 +0000

    basic instict
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ manager.js
@@ -0,0 +1,37 @@
+
+const $task = request('./task.js').base;
+const $user = request('./user.js').base;
+const $project = request('./project.js').base;
+const $timeBox = request('./timeBox.js').base;
+
+function managerBase (){
+    this.newTask = function(){
+
+    }
+    this.task = function(){
+
+    }
+    this.newUser = function(){
+
+    }
+    this.user = function(){
+
+    }
+    this.newProject = function(){
+
+    }
+    this.project = function(){
+
+    }
+    this.newBox = function(){
+
+    }
+    this.box = function(){
+
+    }
+    let _tasks     = {};
+    let _users     = {};
+    let _projects  = {};
+    let _timeBoxes = {};
+
+}
