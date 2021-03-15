cbc9b9dc76bda09ca68db490b84e52598a805618
Soldy <4786022+Soldy@users.noreply.github.com>
2021-03-15 07:38:07 +0000

    box design
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ box.js
@@ -0,0 +1,47 @@
+/*
+ *  @Soldy\temprc\2021.03.15\GPL3
+ */
+'use strict';
+const $setuprc = (require('setuprc')).base;
+
+const box = function(settings){
+    this.start = function(){
+    };
+    this.stop =  function(){
+    };
+    this.history = function(){
+
+    },
+    this.setup = function(){
+        return _setup;
+    };
+    const _setup =  $setuprc({
+        'title':{
+            'type'    : 'string',
+            'defautl' : 'nameless'
+        },
+        'comment':{
+            'type'    : 'string',
+            'default' : ''
+        },
+        'createdA':{
+            'type'    : 'integer',
+            'default' : (Date.now()+75600000)
+        },
+        'user':{
+            'type'    : 'string',
+            'default' : ''
+        },
+        'task':{
+            'type'    : 'string',
+            'default' : ''
+        }
+    });
+    let _history = [];
+    let _current = 0;
+    let _count = 0;
+    if ( typeof settings !== 'undefined' )
+        _setup.setup(settings);
+};
+
+
